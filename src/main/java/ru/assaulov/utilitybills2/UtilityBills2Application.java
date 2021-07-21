a8b400c5e3a9ae02d1faec577ae0bc2d1c6b358e
assa <assaulov.i@gmail.com>
2021-07-21 11:33:12 +0300

    init commit. Added dependencies, start app

---
+++ src/main/java/ru/assaulov/utilitybills2/UtilityBills2Application.java
@@ -0,0 +1,13 @@
+package ru.assaulov.utilitybills2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityBills2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilityBills2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/assaulov/utilitybills2/UtilityBills2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.assaulov.utilitybills2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilityBills2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
