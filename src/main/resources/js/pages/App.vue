743b785bb748446783267f1382946994d00a9f2d
assa <assaulov.i@gmail.com>
2021-07-22 18:13:53 +0300

    App.vue

---
+++ src/main/resources/js/pages/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title>Utility Bills</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn  icon @click="login">Login
+      </v-btn>
+    </v-app-bar>
+
+    <v-main >
+      <router-view></router-view>
+    </v-main>
+
+  </v-app>
+</template>
+
+<script>
+export default {
+methods: {
+  login(){
+    this.$router.push('/login')
+
+  }
+}
+}
+</script>
+
+<style>
+
+</style>
