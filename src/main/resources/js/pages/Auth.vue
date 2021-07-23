ae5a189a24022423bbc26a3cfe30271994ee0ef4
assa <assaulov.i@gmail.com>
2021-07-23 18:15:06 +0300

    Добалена форма регистрации

--- src/main/resources/js/pages/Auth.vue
+++ src/main/resources/js/pages/Auth.vue
@@ -1,12 +1,41 @@
 <template>
-<auth-form class="pa-10 ma-20"></auth-form>
+  <v-container>
+  <v-container>
+  <v-layout>
+  <auth-form v-if="!isRegistrationFormVisible" class="pa-10 ma-20"></auth-form>
+  </v-layout>
+    </v-container>
+  <v-container class="my-20">>
+    <v-col  class="align-self-center">
+      <registration-form v-if="isRegistrationFormVisible"></registration-form>
+    </v-col>
+  </v-container>
+  <v-container>
+    <v-layout>
+    <v-btn v-if="!isRegistrationFormVisible" @click="register" class="pa-10 ma-30">
+      Зарегистрироваться
+    </v-btn>
+    </v-layout>
+  </v-container>
+  </v-container>
 </template>
 
 <script>
 import AuthForm from "../components/login/AuthForm.vue";
+import RegistrationForm from "../components/registration/RegistrationForm.vue";
 export default {
   name: "Auth",
-  components: {AuthForm}
+  components: {RegistrationForm, AuthForm},
+  data(){
+      return{
+        isRegistrationFormVisible: false
+      }
+  },
+  methods:{
+    register(){
+    this.isRegistrationFormVisible=true
+    }
+  }
 }
 </script>
 
--- webpack.dev.js
+++ webpack.dev.js
@@ -11,7 +11,7 @@ module.exports = merge(common, {
         allowedHosts: [
             'localhost:8888'
         ],
-        stats: 'errors-only',
+        stats: 'normal',
         clientLogLevel: 'error',
 
     },
