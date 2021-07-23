ad688139898b9c77237b3adcfa3e01ff06f8a312
assa <assaulov.i@gmail.com>
2021-07-23 20:26:25 +0300

    version = '0.4.0'

--- src/main/java/ru/assaulov/utilitybills2/dto/UserDto.java
+++ src/main/java/ru/assaulov/utilitybills2/dto/UserDto.java
@@ -3,13 +3,31 @@ package ru.assaulov.utilitybills2.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Column;
 
 @Data
-@AllArgsConstructor
 public class UserDto {
 	private String login;
 	private String fullName;
+	private String firstName;
+	private String lastName;
 	private String gender;
 	private String email;
 
+	public UserDto(String login, String fullName, String gender, String email) {
+		this.login = login;
+		this.fullName = fullName;
+		this.gender = gender;
+		this.email = email;
+	}
+
+	public UserDto(String login, String firstName, String lastName, String gender, String email) {
+		this.login = login;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.gender = gender;
+		this.email = email;
+	}
 }
